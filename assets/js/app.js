2848d90077a3b6c867ec1a50f42812e6429a155e
virginiachae <chae.virginia@gmail.com>
2017-02-02 13:52:44 -0800

    made images more transparent on hover

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,17 @@
 // sanity check
 console.log("app.js linked.");
+$(function(){
+    var box = document.getElementsByClassName('box')
+
+    // $box.mouseover(function() {
+    // 	box.addClass('blur');
+    // });
+
+    var $box = $('.box');
+
+    console.log("jquery box is ", $box);
+    console.log("box is ", box);
+
+    var container = $('.container')
+    console.log(container);
+});
